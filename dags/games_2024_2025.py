de5a2bb91e9f2726b210188d444354e991184c0c
VergosNikos <nikos123v@gmail.com>
2025-05-20 18:36:07 +0300

    Formatted files using ruff

--- dags/games_2024_2025.py
+++ dags/games_2024_2025.py
@@ -1,6 +1,5 @@
 import pendulum
 import requests
-import json
 from airflow.decorators import dag, task
 
 
@@ -9,24 +8,25 @@ from airflow.decorators import dag, task
     catchup=False,
     tags=["example"],
 )
-
 def euroleague_games_2024_2025():
-
     @task()
     def get_games_2024():
-        result_raw = requests.get("https://api-live.euroleague.net/v2/competitions/E/seasons/E2023/games")
-        result_data = result_raw.json().get("data")
-        print(result_data)
-        return result_data
-    
-    @task()
-    def get_games_2025():
-        result_raw = requests.get("https://api-live.euroleague.net/v2/competitions/E/seasons/E2024/games")
+        result_raw = requests.get(
+            "https://api-live.euroleague.net/v2/competitions/E/seasons/E2023/games"
+        )
         result_data = result_raw.json().get("data")
         print(result_data)
         return result_data
 
-    
+    @task()
+    def get_games_2025():
+        result_raw = requests.get(
+            "https://api-live.euroleague.net/v2/competitions/E/seasons/E2024/games"
+        )
+        result_data = result_raw.json().get("data")
+        print(result_data)
+        return result_data
+
     games_2024 = get_games_2024()
     games_2025 = get_games_2025()
 
