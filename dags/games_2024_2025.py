6e597739d8f47ed511c2eed38d4db4f3797a2487
VergosNikos <nikos123v@gmail.com>
2025-05-21 19:13:43 +0300

    Added teams for season 2025 to MongoDB

--- dags/games_2024_2025.py
+++ dags/games_2024_2025.py
@@ -3,10 +3,10 @@ import requests
 from airflow.decorators import dag, task
 from core.mongodb.mongo_service import db, sanitize_id
 
+games_2023_collection = db.games_2023
+games_2023_collection.create_index("gameCode", unique=True)
 games_2024_collection = db.games_2024
 games_2024_collection.create_index("gameCode", unique=True)
-games_2025_collection = db.games_2025
-games_2025_collection.create_index("gameCode", unique=True)
 
 
 @dag(
@@ -14,11 +14,40 @@ games_2025_collection.create_index("gameCode", unique=True)
     catchup=False,
     tags=["example"],
 )
-def euroleague_games_2024_2025():
+def euroleague_games_2023_2024():
+    @task()
+    def get_games_2023():
+        result_raw = requests.get(
+            "https://api-live.euroleague.net/v2/competitions/E/seasons/E2023/games"
+        )
+        result_data = result_raw.json().get("data")
+        games_to_be_added = []
+        for game in result_data:
+            game_code = game.get("gameCode")
+            query = {"gameCode": game_code}
+            existing_game = games_2023_collection.find_one(query)
+            if not existing_game:
+                games_to_be_added.append(game)
+        if games_to_be_added:
+            try:
+                games_2023_documents = games_2023_collection.insert_many(
+                    games_to_be_added, ordered=False
+                )
+            except Exception as e:
+                return {"message": f"No documents inserted. Exception: {str(e)}"}
+            sanitized_documents = [
+                sanitize_id(document) for document in games_2023_documents
+            ]
+            return sanitized_documents
+        else:
+            all_games_2023_documents = list(games_2023_collection.find())
+            first_document = all_games_2023_documents[0]
+            return sanitize_id(first_document)
+
     @task()
     def get_games_2024():
         result_raw = requests.get(
-            "https://api-live.euroleague.net/v2/competitions/E/seasons/E2023/games"
+            "https://api-live.euroleague.net/v2/competitions/E/seasons/E2024/games"
         )
         result_data = result_raw.json().get("data")
         games_to_be_added = []
@@ -44,37 +73,8 @@ def euroleague_games_2024_2025():
             first_document = all_games_2024_documents[0]
             return sanitize_id(first_document)
 
-    @task()
-    def get_games_2025():
-        result_raw = requests.get(
-            "https://api-live.euroleague.net/v2/competitions/E/seasons/E2024/games"
-        )
-        result_data = result_raw.json().get("data")
-        games_to_be_added = []
-        for game in result_data:
-            game_code = game.get("gameCode")
-            query = {"gameCode": game_code}
-            existing_game = games_2025_collection.find_one(query)
-            if not existing_game:
-                games_to_be_added.append(game)
-        if games_to_be_added:
-            try:
-                games_2025_documents = games_2025_collection.insert_many(
-                    games_to_be_added, ordered=False
-                )
-            except Exception as e:
-                return {"message": f"No documents inserted. Exception: {str(e)}"}
-            sanitized_documents = [
-                sanitize_id(document) for document in games_2025_documents
-            ]
-            return sanitized_documents
-        else:
-            all_games_2025_documents = list(games_2025_collection.find())
-            first_document = all_games_2025_documents[0]
-            return sanitize_id(first_document)
-
-    games_2024 = get_games_2024()
-    games_2025 = get_games_2025()
+    games_2024 = get_games_2023()
+    games_2025 = get_games_2024()
 
 
-euroleague_dag = euroleague_games_2024_2025()
+euroleague_dag = euroleague_games_2023_2024()
