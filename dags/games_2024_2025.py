c26360f18628838c379a6b7e9173a52f1d3df7a1
VergosNikos <nikos123v@gmail.com>
2025-05-21 16:53:52 +0300

    Added teams to MongoDB

--- dags/games_2024_2025.py
+++ dags/games_2024_2025.py
@@ -6,6 +6,7 @@ from core.mongodb.mongo_service import db, sanitize_id
 games_2024_collection = db.games_2024
 games_2024_collection.create_index("gameCode", unique=True)
 games_2025_collection = db.games_2025
+games_2025_collection.create_index("gameCode", unique=True)
 
 
 @dag(
@@ -49,8 +50,28 @@ def euroleague_games_2024_2025():
             "https://api-live.euroleague.net/v2/competitions/E/seasons/E2024/games"
         )
         result_data = result_raw.json().get("data")
-        print(result_data)
-        return result_data
+        games_to_be_added = []
+        for game in result_data:
+            game_code = game.get("gameCode")
+            query = {"gameCode": game_code}
+            existing_game = games_2025_collection.find_one(query)
+            if not existing_game:
+                games_to_be_added.append(game)
+        if games_to_be_added:
+            try:
+                games_2025_collection = games_2025_collection.instert_many(
+                    games_to_be_added, ordered=False
+                )
+            except Exception as e:
+                return {"message": f"No documents inserted. Exception: {str(e)}"}
+            sanitized_documents = [
+                sanitize_id(document) for document in all_games_2025_documents
+            ]
+            return sanitized_documents
+        else:
+            all_games_2025_documents = list(games_2025_collection.find())
+            first_document = all_games_2025_documents[0]
+            return sanitize_id(first_document)
 
     games_2024 = get_games_2024()
     games_2025 = get_games_2025()
