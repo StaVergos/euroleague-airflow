9b2987f27b1bea16cd9e75b3a97a595d6f26881b
VergosNikos <nikos123v@gmail.com>
2025-05-23 20:07:11 +0300

    Added players to mongodb for 2 last seasons

--- dags/games_2024_2025.py
+++ dags/games_2024_2025.py
@@ -1,7 +1,7 @@
 import pendulum
 import requests
 from airflow.decorators import dag, task
-from core.mongodb.mongo_service import db, sanitize_id, parse_dict
+from core.mongodb.mongo_service import db, sanitize_id
 from pymongo.errors import BulkWriteError
 
 games_2023_collection = db.games_2023
@@ -10,9 +10,9 @@ games_2024_collection = db.games_2024
 games_2024_collection.create_index("gameCode", unique=True)
 
 players_2023_collection = db.players_2023
-players_2023_collection.create_index("person.code", unique=True)
+players_2023_collection.create_index("person_code", unique=True)
 players_2024_collection = db.players_2024
-players_2024_collection.create_index("person.code", unique=True)
+players_2024_collection.create_index("person_code", unique=True)
 
 
 @dag(
@@ -83,21 +83,76 @@ def euroleague_games_2023_2024():
             "https://api-live.euroleague.net/v2/competitions/E/seasons/E2023/people"
         )
         result_data = result_raw.json().get("data")
-        flatten_list = []
-        for dictionary in result_data:
-            flat_dictionary = parse_dict(dictionary)
-            flatten_list.append(flat_dictionary)
+        flat_data = []
+        for player in result_data:
+            player = {
+                "person_code": player["person"]["code"],
+                "person_name": player["person"]["name"],
+                "person_alias": player["person"]["alias"],
+                "person_aliasRaw": player["person"]["aliasRaw"],
+                "person_passportName": player["person"]["passportName"],
+                "person_passportSurname": player["person"]["passportSurname"],
+                "person_jerseyName": player["person"]["jerseyName"],
+                "person_abbreviatedName": player["person"]["abbreviatedName"],
+                "person_country_code": (
+                    (player.get("person") or {}).get("country") or {}
+                ).get("code", None),
+                "person_country_name": (
+                    (player.get("person") or {}).get("country") or {}
+                ).get("name", None),
+                "person_height": player["person"]["height"],
+                "person_weight": player["person"]["weight"],
+                "person_birthDate": player["person"]["birthDate"],
+                "person_birthCountry_code": (
+                    (player.get("person") or {}).get("birthCountry") or {}
+                ).get("code", None),
+                "person_birthCountry_name": (
+                    (player.get("person") or {}).get("birthCountry") or {}
+                ).get("name", None),
+                "person_twitterAccount": player["person"]["twitterAccount"],
+                "person_instagramAccount": player["person"]["instagramAccount"],
+                "person_facebookAccount": player["person"]["facebookAccount"],
+                "person_isReferee": player["person"]["isReferee"],
+                "person_images": player["person"]["images"],
+                "type": player["type"],
+                "typeName": player["typeName"],
+                "active": player["active"],
+                "startDate": player["startDate"],
+                "endDate": player["endDate"],
+                "order": player["order"],
+                "dorsal": player["dorsal"],
+                "dorsalRaw": player["dorsalRaw"],
+                "position": player["position"],
+                "positionName": player["positionName"],
+                "lastTeam": player["lastTeam"],
+                "externalId": player["externalId"],
+                "images": player["images"],
+                "club_code": player["club"]["code"],
+                "club_name": player["club"]["name"],
+                "club_abbreviatedName": player["club"]["abbreviatedName"],
+                "club_editorialName": player["club"]["editorialName"],
+                "club_tvCode": player["club"]["tvCode"],
+                "club_isVirtual": player["club"]["isVirtual"],
+                "club_images": player["club"]["images"]["crest"],
+                "season_name": player["season"]["name"],
+                "season_code": player["season"]["code"],
+                "season_alias": player["season"]["alias"],
+                "season_competitionCode": player["season"]["competitionCode"],
+                "season_year": player["season"]["year"],
+                "season_startDate": player["season"]["startDate"],
+            }
+            flat_data.append(player)
         players_to_be_added = []
-        for player in flatten_list:
-            player_code = player.get("person.code")
-            query = {"person.code": player_code}
+        for player in flat_data:
+            player_code = player.get("person_code")
+            query = {"person_code": player_code}
             existing_player = players_2023_collection.find_one(query)
             if not existing_player:
                 players_to_be_added.append(player)
         if players_to_be_added:
             try:
                 players_2023_documents = players_2023_collection.insert_many(
-                    players_to_be_added, ordered=False
+                    players_to_be_added
                 )
             except BulkWriteError as e:
                 return {
@@ -115,21 +170,76 @@ def euroleague_games_2023_2024():
             "https://api-live.euroleague.net/v2/competitions/E/seasons/E2024/people"
         )
         result_data = result_raw.json().get("data")
-        flatten_list = []
-        for dictionary in result_data:
-            flat_dictionary = parse_dict(dictionary)
-            flatten_list.append(flat_dictionary)
+        flat_data = []
+        for player in result_data:
+            player = {
+                "person_code": player["person"]["code"],
+                "person_name": player["person"]["name"],
+                "person_alias": player["person"]["alias"],
+                "person_aliasRaw": player["person"]["aliasRaw"],
+                "person_passportName": player["person"]["passportName"],
+                "person_passportSurname": player["person"]["passportSurname"],
+                "person_jerseyName": player["person"]["jerseyName"],
+                "person_abbreviatedName": player["person"]["abbreviatedName"],
+                "person_country_code": (
+                    (player.get("person") or {}).get("country") or {}
+                ).get("code", None),
+                "person_country_name": (
+                    (player.get("person") or {}).get("country") or {}
+                ).get("name", None),
+                "person_height": player["person"]["height"],
+                "person_weight": player["person"]["weight"],
+                "person_birthDate": player["person"]["birthDate"],
+                "person_birthCountry_code": (
+                    (player.get("person") or {}).get("birthCountry") or {}
+                ).get("code", None),
+                "person_birthCountry_name": (
+                    (player.get("person") or {}).get("birthCountry") or {}
+                ).get("name", None),
+                "person_twitterAccount": player["person"]["twitterAccount"],
+                "person_instagramAccount": player["person"]["instagramAccount"],
+                "person_facebookAccount": player["person"]["facebookAccount"],
+                "person_isReferee": player["person"]["isReferee"],
+                "person_images": player["person"]["images"],
+                "type": player["type"],
+                "typeName": player["typeName"],
+                "active": player["active"],
+                "startDate": player["startDate"],
+                "endDate": player["endDate"],
+                "order": player["order"],
+                "dorsal": player["dorsal"],
+                "dorsalRaw": player["dorsalRaw"],
+                "position": player["position"],
+                "positionName": player["positionName"],
+                "lastTeam": player["lastTeam"],
+                "externalId": player["externalId"],
+                "images": player["images"],
+                "club_code": player["club"]["code"],
+                "club_name": player["club"]["name"],
+                "club_abbreviatedName": player["club"]["abbreviatedName"],
+                "club_editorialName": player["club"]["editorialName"],
+                "club_tvCode": player["club"]["tvCode"],
+                "club_isVirtual": player["club"]["isVirtual"],
+                "club_images": player["club"]["images"]["crest"],
+                "season_name": player["season"]["name"],
+                "season_code": player["season"]["code"],
+                "season_alias": player["season"]["alias"],
+                "season_competitionCode": player["season"]["competitionCode"],
+                "season_year": player["season"]["year"],
+                "season_startDate": player["season"]["startDate"],
+            }
+            flat_data.append(player)
         players_to_be_added = []
-        for player in flatten_list:
-            player_code = player.get("person.code")
-            query = {"person.code": player_code}
+        for player in flat_data:
+            player_code = player.get("person_code")
+            query = {"person_code": player_code}
             existing_player = players_2024_collection.find_one(query)
             if not existing_player:
                 players_to_be_added.append(player)
         if players_to_be_added:
             try:
                 players_2024_documents = players_2024_collection.insert_many(
-                    players_to_be_added, ordered=False
+                    players_to_be_added
                 )
             except BulkWriteError as e:
                 return {
